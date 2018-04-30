ebff1a099fcfc33c1fd0ae8779db9d40307ebf0c
MariuszGebel <gebel@wp.pl>
2018-04-30 21:51:05 +0200

    First Commit

---
+++ src/main/java/com/kodilla/KodillaCourseApplication.java
@@ -0,0 +1,14 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourseApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo world");
+        System.out.println("Hallo świecie");
+    }
+
+}
